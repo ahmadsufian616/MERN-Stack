893c3c65d646f700cc2066332bfa5e3a31c3d816
ahmadsufian616 <ahmad11213731@gmail.com>
2021-07-13 14:28:30 +0300

    new

---
+++ React v3 (Dev)/FirstReact/firstreact/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+
+        <
+        div className = "class" >
+        <
+        h1 > Hello Dojo! < /h1> <
+        h2 > Things i need to do : < /h2> <
+            ul >
+            <
+            li > Learn React < /li> <
+            li > climb mt Everest < /li> <
+            li > run a marthon < /li> <
+            li > feed the do </li> <
+                /ul> <
+                /div>
+
+    );
+}
+
+export default App;
